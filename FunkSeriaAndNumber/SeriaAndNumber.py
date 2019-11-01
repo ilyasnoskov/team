67befc5935e96b0324e3012811f45bed4d885054
Bodia <lalala@gmail.com>
2019-11-01 11:59:43 +0200

    add SeriaAndNumber

---
+++ FunkSeriaAndNumber/SeriaAndNumber.py
@@ -0,0 +1,11 @@
+def seria_number(text=''):
+    """"
+    :param text:str, format, date, КВ(кв) 13213662
+    :return:str,format date, КВ №13213662
+
+    """
+    return None
+
+
+if __name__ == '__main__':
+    seria_number('some data')
